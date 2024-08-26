0f70708a488a6361737e3a653d4f4f5de4131e00
ALineNNLopes <alinennlopes1522@gmail.com>
2024-08-26 09:19:55 -0300

    exercicio joy

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello Word");
+let num3 = 15;
+let num4 = 27;
+console.log(num3 + num4)
+
+const conectar = "Conexão feita com sucesso!"
+console.log (conectar);
+console.log (conectar);
+console.log (conectar);
